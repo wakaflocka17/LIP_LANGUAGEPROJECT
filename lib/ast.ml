a7ec832998151fca367ebc93a5652bdecd55c184
Francesco Congiu <f.congiu38@studenti.unica.it>
2023-02-06 16:45:18 +0100

    Test

---
+++ lib/ast.ml
@@ -0,0 +1,15 @@
+type ide = string (* Ide -> name variable *)
+
+(* Dichiarazioni Variabili *)
+
+(* Dichiarazioni Procedure *)
+
+(* Espressioni *)
+
+(* Comandi *)
+
+(* Parametri formali *)
+
+(* Parametri attuale *)
+
+(* Programma *)
