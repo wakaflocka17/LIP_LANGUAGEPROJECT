3e82c4f462a4858ef4dd7cd8865815d2951e8256
Mambo <m.dessalvi02@gmail.com>
2023-02-06 17:50:00 +0100

    Added types for declaration of variables

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,6 +1,11 @@
 type ide = string (* Ide -> name variable *)
 
 (* Dichiarazioni Variabili *)
+type dv = 
+  | nullvar
+  | seq_dv of dv * dv       (* Multiple declarations *)
+  | var of ide              (* Declaration of a variable *)
+  | array of ide * int      (* Declaration of an array *)
 
 (* Dichiarazioni Procedure *)
 
